0d3d97348188d11e2bae7bce045847165243c6e0
nazarbaev <59479158+sarry13@users.noreply.github.com>
2021-01-01 02:08:24 +0600

    optimization

--- lua/bg_citizens_core/sv_npc_remover.lua
+++ lua/bg_citizens_core/sv_npc_remover.lua
@@ -1,14 +1,14 @@
 timer.Create('bgCitizensRemover', 1, 0, function()
     if #bgCitizens.npcs ~= 0 then
-        for _, npc in pairs(bgCitizens.npcs) do
+        for _, npc in ipairs(bgCitizens.npcs) do
             if IsValid(npc) and npc:Health() > 0 then
                 local isRemove = true
 
-                for _, ply in pairs(player.GetAll()) do
+                for _, ply in ipairs(player.GetAll()) do
                     if IsValid(ply) then
                         local npcPos = npc:GetPos()
                         local plyPos = ply:GetPos()
-                        if npcPos:Distance(plyPos) < 3000 or bgCitizens:PlayerIsViewVector(ply, npcPos) then
+                        if npcPos:DistToSqr(plyPos) < 3000000 or bgCitizens:PlayerIsViewVector(ply, npcPos) then
                             isRemove = false
                             break
                         end
@@ -25,7 +25,7 @@ timer.Create('bgCitizensRemover', 1, 0, function()
         end
 
         local new_table = {}
-        for _, npc in pairs(bgCitizens.npcs) do
+        for _, npc in ipairs(bgCitizens.npcs) do
             if IsValid(npc) and npc:Health() > 0 then
                 table.insert(new_table, npc)
             end
