d2cab53661a2c53ade228901b2556e86d444de02
Shark-vil <shark_vil@mail.ru>
2021-01-04 01:12:35 +0300

    Added pushing back the player if he is face to face with the police

--- lua/bg_citizens_core/actions/sv_open_door.lua
+++ lua/bg_citizens_core/actions/sv_open_door.lua
@@ -1,46 +1,35 @@
-timer.Create('bgCitizensTimerOpenDoorAction', 1, 0, function()
-    for _, actor in pairs(bgCitizens:GetAll()) do
-        local npc = actor:GetNPC()
-        if IsValid(npc) then
-            local door_class = {
-				"func_door",
-				"func_door_rotating",
-				"prop_door_rotating",
-				"func_movelinear",
-				"prop_dynamic",
-            }
-            
-            local tr = util.TraceLine({
-                start = npc:EyePos(),
-                endpos = npc:EyePos() + npc:EyeAngles():Forward() * 100,
-                filter = function(ent) 
-                    if table.HasValue(door_class, ent:GetClass()) then
-                        return true
-                    end
-                end
-            })
+hook.Add("bgCitizens_ObjectBeforeNPC", "bgCitizensOpenDoorAction", function(actor, door)
+    local door_class = {
+        "func_door",
+        "func_door_rotating",
+        "prop_door_rotating",
+        "func_movelinear",
+        "prop_dynamic",
+    }
 
-            local door = tr.Entity
+    if not table.HasValue(door_class, door:GetClass()) then return end
 
-            if tr.Hit and IsValid(door) and hook.Run('bgCitizens_PreOpenDoor', npc, door) == nil then
-                actor:PlayStaticSequence('Open_door_away')
+    local npc = actor:GetNPC()
 
-                door.bgCitizenOpenDoor = true
-                
-                door:Fire("unlock", "", 0)
-                door:Fire("open", "", 0)
-                
-                hook.Run('bgCitizens_PostOpenDoor', npc, door)
+    if hook.Run('bgCitizens_PreOpenDoor', npc, door) == nil then
+        actor:PlayStaticSequence('Open_door_away')
 
-                timer.Simple(10, function()
-                    if IsValid(door) and door.bgCitizenOpenDoor and 
-                        hook.Run('bgCitizens_PreCloseDoor', door) == nil
-                    then
-                        door:Fire("close", "", 0)
-                        door.bgCitizenOpenDoor = false
-                    end
-                end)
+        door.bgCitizenOpenDoor = true
+        
+        door:Fire("unlock", "", 0)
+        door:Fire("open", "", 0)
+        
+        hook.Run('bgCitizens_PostOpenDoor', npc, door)
+
+        timer.Simple(10, function()
+            if IsValid(door) and door.bgCitizenOpenDoor and 
+                hook.Run('bgCitizens_PreCloseDoor', door) == nil
+            then
+                door:Fire("close", "", 0)
+                door.bgCitizenOpenDoor = false
+
+                hook.Run('bgCitizens_PostCloseDoor', door)
             end
-        end
+        end)
     end
 end)
--- lua/bg_citizens_core/sv_npc_creator.lua
+++ lua/bg_citizens_core/sv_npc_creator.lua
@@ -33,7 +33,6 @@ hook.Add("Think", "bgCitizensLoopAnimatorController", function()
                 npc:SetNPCState(NPC_STATE_SCRIPT)
                 npc:SetSchedule(SCHED_SLEEP)
 
-                -- print(tostring(actor.anim_is_loop) .. ' - ' .. actor.anim_name)
                 if actor:IsLoopSequence() then
                     if actor:IsSequenceLoopFinished() then
                         actor:ResetSequence()
@@ -44,11 +43,62 @@ hook.Add("Think", "bgCitizensLoopAnimatorController", function()
                 elseif actor:IsSequenceFinished() then
                     actor:ResetSequence()
                 end
-
-                -- print(actor.anim_name)
             end
         end
     end
 end)
 
--- hook.Remove("Tick", "bgCitizensLoopAnimatorController")
+timer.Create('bgCitizensTimerObjectBeforeNPCAction', 1, 0, function()
+    for _, actor in ipairs(bgCitizens:GetAll()) do
+        local npc = actor:GetNPC()
+        if IsValid(npc) then
+            local npc = actor:GetNPC()
+        
+            local tr = util.TraceLine({
+                start = npc:EyePos(),
+                endpos = npc:EyePos() + npc:EyeAngles():Forward() * 100,
+                filter = function(ent) 
+                    if ent ~= npc then return true end
+                end
+            })
+
+            if tr.Hit and IsValid(tr.Entity) then
+                hook.Run('bgCitizens_ObjectBeforeNPC', actor, tr.Entity)
+            end
+        end
+    end
+end)
+
+hook.Add("bgCitizens_ObjectBeforeNPC", "PolicePushAnotherNPC", function(actor, ent)
+    if ent:IsPlayer() and actor:GetType() == 'police' 
+        and actor:GetState() == 'walk'
+        and actor:IsSequenceFinished() and math.random(0, 100) <= 60 
+    then
+        if ent:GetPos():DistToSqr(actor:GetNPC():GetPos()) > 50 ^ 2 then return end
+
+        actor:PlayStaticSequence('LuggagePush')
+
+        local forward = actor:GetNPC():GetForward()
+
+        local angle_punch_pitch = math.Rand(-20, 20)
+        local angle_punch_yaw = math.sqrt(20 * 20 - angle_punch_pitch * angle_punch_pitch)
+        if math.random(0, 1) == 1 then
+            angle_punch_yaw = angle_punch_yaw * -1
+        end
+        ent:ViewPunch(Angle(angle_punch_pitch, angle_punch_yaw, 0))
+
+        local slapSounds = {
+            "physics/body/body_medium_impact_hard1.wav",
+            "physics/body/body_medium_impact_hard2.wav",
+            "physics/body/body_medium_impact_hard3.wav",
+            "physics/body/body_medium_impact_hard5.wav",
+            "physics/body/body_medium_impact_hard6.wav",
+            "physics/body/body_medium_impact_soft5.wav",
+            "physics/body/body_medium_impact_soft6.wav",
+            "physics/body/body_medium_impact_soft7.wav",
+        }
+
+        ent:EmitSound(table.Random(slapSounds), 75, 100, 0.3, CHAN_AUTO)
+        ent:SetVelocity(forward * 250)
+    end
+end)
