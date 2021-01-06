a163e2f2a218a9b9eaddd8366e7f1a99a2c28376
Shark-vil <shark_vil@mail.ru>
2021-01-06 06:27:16 +0300

    Dropping out money from NPC (DarkRP)

--- lua/bg_citizens_core/modules/sv_darkrp_drop_money.lua
+++ lua/bg_citizens_core/modules/sv_darkrp_drop_money.lua
@@ -8,7 +8,7 @@ hook.Add("bgCitizens_OnKilledActor", "bgCitizensDropMoney", function(actor)
         local pos = npc:GetPos()
         if data.money ~= nil then
             local money = math.random(data.money[1], data.money[2])
-            -- next update
+            DarkRP.createMoneyBag(pos, money)
         end
     end
 end)
