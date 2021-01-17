d912228350c3c15e456b1f94324be41282df2157
Shark-vil <shark_vil@mail.ru>
2021-01-17 07:49:23 +0300

    Improved dialog binding

--- lua/bg_citizens_core/modules/quest_dialogue/sv_parent_dialogue.lua
+++ lua/bg_citizens_core/modules/quest_dialogue/sv_parent_dialogue.lua
@@ -6,3 +6,27 @@ hook.Add("BGN_PostSpawnNPC", "BGN_QuestSystem_DialogueParent", function(actor)
 
     QuestDialogue:AutoParentToNPC(actor:GetNPC())
 end)
+
+hook.Add("QSystem.StartDialogue", "BGN_QuestSystem_StartDialogue", function(eDialogue)
+    if eDialogue:GetDialogue().isBackground then return end
+
+    local npc = eDialogue:GetNPC()
+    if IsValid(npc) and npc:IsNPC() then
+        local actor = bgNPC:GetActor(npc)
+        if actor ~= nil then
+            actor:SetState('quest_dialogue')
+        end
+    end
+end)
+
+hook.Add("QSystem.StopDialogue", "BGN_QuestSystem_StopDialogue", function(eDialogue)
+    if eDialogue:GetDialogue().isBackground then return end
+    
+    local npc = eDialogue:GetNPC()
+    if IsValid(npc) and npc:IsNPC() then
+        local actor = bgNPC:GetActor(npc)
+        if actor ~= nil then
+            actor:SetOldState()
+        end
+    end
+end)
