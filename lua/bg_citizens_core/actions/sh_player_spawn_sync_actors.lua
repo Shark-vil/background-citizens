3b0c4159a9b6da671ebebcf921ffd83896181a2c
Shark-vil <shark_vil@mail.ru>
2021-01-24 22:57:16 +0300

    Revert "Changing sync timers"
    This reverts commit a3cbd1889f5585088d66d4463679405462ff5747.

--- lua/bg_citizens_core/actions/sh_player_spawn_sync_actors.lua
+++ lua/bg_citizens_core/actions/sh_player_spawn_sync_actors.lua
@@ -5,27 +5,28 @@ if SERVER then
 		timer.Simple(3, function()
 			if not IsValid(ply) then return end
 
+			local sync_time = 1
+
 			for _, actor in ipairs(bgNPC:GetAll()) do
 				if actor:IsAlive() then
 					local type = actor:GetType()
 					local npc = actor:GetNPC()
+					bgNPC:TemporaryVectorVisibility(npc, 3)
 
-					if IsValid(npc) then
-						bgNPC:TemporaryVectorVisibility(npc, 3)
+					timer.Simple(sync_time, function()
+						if not IsValid(npc) or not IsValid(ply) then return end
+
+						net.InvokeAll('bgn_add_actor_from_client', type, npc)
 
 						timer.Simple(1, function()
 							if not IsValid(npc) or not IsValid(ply) then return end
 
-							net.InvokeAll('bgn_add_actor_from_client', type, npc)
-
-							timer.Simple(1.5, function()
-								if not IsValid(npc) or not IsValid(ply) then return end
-
-								actor:SyncData()
-								bgNPC.Log('Actor [' .. type .. '] - ' .. tostring(npc), ' | Player - ' .. tostring(ply), 'Sync Actors')
-							end)
+							actor:SyncData()
+							bgNPC.Log('Actor [' .. type .. '] - ' .. tostring(npc), ' | Player - ' .. tostring(ply), 'Sync Actors')
 						end)
-					end
+					end)
+					
+					sync_time = sync_time + 0.05
 				end
 			end
 		end)
@@ -36,6 +37,5 @@ if SERVER then
 else
 	net.RegisterCallback('bgn_is_loaded_setup', function()
 		LocalPlayer().BGN_IsLoaded = true
-		MsgN('!!!!!!!!!! BGN_IsLoaded !!!!!!!!!!')
 	end)
 end
