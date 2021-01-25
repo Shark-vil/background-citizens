3e156eb7df91a542a92f85fb3dab64892779bbde
Shark-vil <shark_vil@mail.ru>
2021-01-25 04:20:32 +0300

    Improving the readability of the config - sh_arrest

--- lua/bg_citizens_core/config/states/sh_arrest.lua
+++ lua/bg_citizens_core/config/states/sh_arrest.lua
@@ -1,6 +1,10 @@
 bgNPC.cfg.arrest = {}
+
+-- TEXT --
 bgNPC.cfg.arrest['order_text'] = 'Put your head down!'
-bgNPC.cfg.arrest['order_sound'] = 'npc/metropolice/vo/firstwarningmove.wav'
 bgNPC.cfg.arrest['warning_text'] = 'Stay in this position, don\'t move!'
-bgNPC.cfg.arrest['warning_sound'] = 'npc/metropolice/vo/apply.wav'
 bgNPC.cfg.arrest['arrest_notify'] = 'Arrest after %time% sec.'
+
+-- SOUND --
+bgNPC.cfg.arrest['order_sound'] = 'npc/metropolice/vo/firstwarningmove.wav'
+bgNPC.cfg.arrest['warning_sound'] = 'npc/metropolice/vo/apply.wav'
