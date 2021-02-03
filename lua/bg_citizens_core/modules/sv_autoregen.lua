113dcee9bb20a5f571dc3a170ad8894c7c0c8d67
Shark-vil <shark_vil@mail.ru>
2021-02-03 06:17:46 +0300

    Increased gangster regeneration delay

--- lua/bg_citizens_core/modules/sv_autoregen.lua
+++ lua/bg_citizens_core/modules/sv_autoregen.lua
@@ -1,14 +1,17 @@
-timer.Create('BGN_Timer_BanditsAutoRegen', 1, 0, function()
+timer.Create('BGN_Timer_BanditsAutoRegen', 3, 0, function()
    for _, actor in ipairs(bgNPC:GetAllByTeam('bandits')) do
       if actor:IsAlive() then
          local npc = actor:GetNPC()
          local max_health = npc:GetMaxHealth()
          local health = npc:Health()
-         local add_health = math.random(1, 5)
-         local new_health = health + add_health
+         local add_health = math.random(0, 5)
 
-         if health < max_health and new_health <= max_health then
-            npc:SetHealth(new_health)
+         if add_health ~= 0 then
+            local new_health = health + add_health
+
+            if health < max_health and new_health <= max_health then
+               npc:SetHealth(new_health)
+            end
          end
       end
    end
