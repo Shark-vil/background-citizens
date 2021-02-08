df274ce71166df7c1592c14e2e05adcf00fe727d
Shark-vil <shark_vil@mail.ru>
2021-02-08 04:52:39 +0300

    Debug log for cvar

--- lua/bg_citizens_core/modules/extend/cvars/sh_global_cvars.lua
+++ lua/bg_citizens_core/modules/extend/cvars/sh_global_cvars.lua
@@ -58,44 +58,50 @@ if SERVER then
 	end)
 else
 	local cvar_locker = {}
+	local cvar_locker_another = {}
 
 	net.Receive('bgn_gcvars_register_all_from_client', function()
 		bgNPC.GlobalCvars = net.ReadTable()
 		for cvar_name, cvar_data in pairs(bgNPC.GlobalCvars) do
-			if not tobool(GetConVar(cvar_name)) then
-				CreateConVar(cvar_name, cvar_data.value, cvar_data.flag, 
+			CreateConVar(cvar_name, cvar_data.value, cvar_data.flag, 
 					cvar_data.helptext, cvar_data.min, cvar_data.max)
-					
-				cvar_locker[cvar_name] = cvar_locker[cvar_name] or false
-	
-				cvars.AddChangeCallback(cvar_name, function(convar_name, value_old, value_new)
-					if value_old == value_new then return end
-					
+
+			RunConsoleCommand(cvar_name, cvar_data.value)
+			bgNPC:Log('Sync [' .. cvar_name .. ']: ' .. cvar_data.value, 'Cvars')
+
+			cvar_locker[cvar_name] = cvar_locker[cvar_name] or false
+
+			cvars.AddChangeCallback(cvar_name, function(convar_name, value_old, value_new)
+				if value_old == value_new then return end
+				
+				if not cvar_locker_another[convar_name] then
 					if not LocalPlayer():IsAdmin() and not LocalPlayer():IsSuperAdmin() then
-						if not cvar_locker[cvar_name] then
-							cvar_locker[cvar_name] = true
-							timer.Create('bgn_timer_back_cvar_' .. cvar_name, 0.1, 1, function()
-								if not cvar_locker[cvar_name] then return end
-								RunConsoleCommand(cvar_name, value_old)
-								timer.Create('bgn_timer_reset_back_cvar_' .. cvar_name, 0.1, 1, function()
-									if not cvar_locker[cvar_name] then return end
-									cvar_locker[cvar_name] = false
+						if not cvar_locker[convar_name] then
+							cvar_locker[convar_name] = true
+							timer.Create('bgn_timer_back_cvar_' .. convar_name, 0.1, 1, function()
+								if not cvar_locker[convar_name] then return end
+								RunConsoleCommand(convar_name, value_old)
+								timer.Create('bgn_timer_reset_back_cvar_' .. convar_name, 0.1, 1, function()
+									if not cvar_locker[convar_name] then return end
+									cvar_locker[convar_name] = false
 								end)
 							end)
 						end
 						return
 					end
+				end
 
-					bgNPC.GlobalCvars[cvar_name].value = value_new
-					
-					if cvar_locker[cvar_name] then return end
+				bgNPC.GlobalCvars[convar_name].value = value_new
+				bgNPC:Log('Update global cvar [' .. convar_name .. ']: ' .. value_new, 'Cvars')
+				
+				if cvar_locker[convar_name] or cvar_locker_another[convar_name] then return end
 
-					net.Start('bgn_gcvars_change_from_server')
-					net.WriteString(cvar_name)
-					net.WriteFloat(value_new)
-					net.SendToServer()
-				end)
-			end
+				bgNPC:Log('Send to server  [' .. convar_name .. ']', 'Cvars')
+				net.Start('bgn_gcvars_change_from_server')
+				net.WriteString(convar_name)
+				net.WriteFloat(value_new)
+				net.SendToServer()
+			end)
 		end
 	end)
 
@@ -103,13 +109,13 @@ else
 		local cvar_name = net.ReadString()
 		local value = net.ReadFloat()
 
-		cvar_locker[cvar_name] = true
-
+		cvar_locker_another[cvar_name] = true
 		RunConsoleCommand(cvar_name, value)
+		bgNPC:Log('Another sync [' .. cvar_name .. ']: ' .. value, 'Cvars')
 
-		timer.Create('bgn_timer_reset_cvar_locker_' .. cvar_name, 0.2, 1, function()
-			if not cvar_locker[cvar_name] then return end
-			cvar_locker[cvar_name] = false
+		timer.Create('bgn_timer_reset_cvar_another_locker_' .. cvar_name, 0.2, 1, function()
+			if not cvar_locker_another[cvar_name] then return end
+			cvar_locker_another[cvar_name] = false
 		end)
 	end)
 end
