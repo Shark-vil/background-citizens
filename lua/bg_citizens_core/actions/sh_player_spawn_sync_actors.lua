e6201146ac4a165a8b8c5c73b4e53939483a129a
Shark-vil <shark_vil@mail.ru>
2021-02-08 09:34:40 +0300

    Change network callback to hook

--- lua/bg_citizens_core/actions/sh_player_spawn_sync_actors.lua
+++ lua/bg_citizens_core/actions/sh_player_spawn_sync_actors.lua
@@ -1,51 +1,43 @@
 if SERVER then
-	util.AddNetworkString('network_bgn_client_first_initializate')
+	hook.Add("PlayerSpawn", "BGN_PlayerFirstInitSpawnerHook", function(ply)
+		timer.Simple(3, function()
+			if not IsValid(ply) or ply.BGN_IsLoaded then return end
 
-	net.Receive('network_bgn_client_first_initializate', function(len, ply)
-		if ply.BGN_IsLoaded then return end
+			local sync_time = 2
 
-		local sync_time = 2
+			for _, actor in ipairs(bgNPC:GetAll()) do
+				if actor:IsAlive() then
+					local type = actor:GetType()
+					local npc = actor:GetNPC()
+					bgNPC:TemporaryVectorVisibility(npc, 3 + (sync_time - 2))
 
-		for _, actor in ipairs(bgNPC:GetAll()) do
-			if actor:IsAlive() then
-				local type = actor:GetType()
-				local npc = actor:GetNPC()
-				bgNPC:TemporaryVectorVisibility(npc, 3 + (sync_time - 2))
-
-				timer.Simple(sync_time, function()
-					if not IsValid(npc) or not IsValid(ply) then return end
-
-					net.InvokeAll('bgn_add_actor_from_client', type, npc)
-
-					timer.Simple(0.5, function()
+					timer.Simple(sync_time, function()
 						if not IsValid(npc) or not IsValid(ply) then return end
 
-						actor:SyncData(ply)
-						bgNPC.Log('Actor [' .. type .. '] - ' .. tostring(npc) .. ' | Player - ' .. tostring(ply), 'Sync Actors')
+						net.InvokeAll('bgn_add_actor_from_client', type, npc)
+
+						timer.Simple(0.5, function()
+							if not IsValid(npc) or not IsValid(ply) then return end
+
+							actor:SyncData(ply)
+							bgNPC.Log('Actor [' .. type .. '] - ' .. tostring(npc) .. ' | Player - ' .. tostring(ply), 'Sync Actors')
+						end)
 					end)
-				end)
-				
-				sync_time = sync_time + 0.05
+					
+					sync_time = sync_time + 0.05
+				end
 			end
-		end
 
-		hook.Run('BGN_PlayerIsLoaded', ply)
+			hook.Run('BGN_PlayerIsLoaded', ply)
 
-		timer.Simple(1, function()
-			if not IsValid(ply) then return end
-			ply.BGN_IsLoaded = true
-			net.Invoke('bgn_is_loaded_setup', ply)
+			timer.Simple(1, function()
+				if not IsValid(ply) then return end
+				ply.BGN_IsLoaded = true
+				net.Invoke('bgn_is_loaded_setup', ply)
+			end)
 		end)
 	end)
 else
-	hook.Add("InitPostEntity", "BGN_ClientFirstInitializate", function()
-		timer.Simple(1, function()
-			net.Start('network_bgn_client_first_initializate')
-			net.SendToServer()
-		end)
-		hook.Remove("InitPostEntity", "BGN_ClientFirstInitializate")
-	end)
-
 	net.RegisterCallback('bgn_is_loaded_setup', function()
 		LocalPlayer().BGN_IsLoaded = true
 
