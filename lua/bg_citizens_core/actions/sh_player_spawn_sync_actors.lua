fcfad244e60503bf3c261aa6b1e4871ca1a46c8d
Shark-vil <shark_vil@mail.ru>
2021-02-09 08:25:48 +0300

    disable sync after spawn

--- lua/bg_citizens_core/actions/sh_player_spawn_sync_actors.lua
+++ lua/bg_citizens_core/actions/sh_player_spawn_sync_actors.lua
@@ -5,30 +5,30 @@ if SERVER then
 
 			bgNPC:Log('Pre first spawn', 'PlayerSpawner')
 
-			local sync_time = 2
+			-- local sync_time = 2
 
-			for _, actor in ipairs(bgNPC:GetAll()) do
-				if actor:IsAlive() then
-					local type = actor:GetType()
-					local npc = actor:GetNPC()
-					bgNPC:TemporaryVectorVisibility(npc, 3 + (sync_time - 2))
+			-- for _, actor in ipairs(bgNPC:GetAll()) do
+			-- 	if actor:IsAlive() then
+			-- 		local type = actor:GetType()
+			-- 		local npc = actor:GetNPC()
+			-- 		bgNPC:TemporaryVectorVisibility(npc, 3 + (sync_time - 2))
 
-					timer.Simple(sync_time, function()
-						if not IsValid(npc) or not IsValid(ply) then return end
+			-- 		timer.Simple(sync_time, function()
+			-- 			if not IsValid(npc) or not IsValid(ply) then return end
 
-						snet.InvokeAll('bgn_add_actor_from_client', type, npc)
+			-- 			snet.InvokeAll('bgn_add_actor_from_client', type, npc)
 
-						timer.Simple(0.5, function()
-							if not IsValid(npc) or not IsValid(ply) then return end
+			-- 			timer.Simple(0.5, function()
+			-- 				if not IsValid(npc) or not IsValid(ply) then return end
 
-							actor:SyncData(ply)
-							bgNPC.Log('Actor [' .. type .. '] - ' .. tostring(npc) .. ' | Player - ' .. tostring(ply), 'Sync Actors')
-						end)
-					end)
+			-- 				actor:SyncData(ply)
+			-- 				bgNPC.Log('Actor [' .. type .. '] - ' .. tostring(npc) .. ' | Player - ' .. tostring(ply), 'Sync Actors')
+			-- 			end)
+			-- 		end)
 					
-					sync_time = sync_time + 0.05
-				end
-			end
+			-- 		sync_time = sync_time + 0.05
+			-- 	end
+			-- end
 
 			hook.Run('BGN_PlayerIsLoaded', ply)
 			ply.BGN_IsLoaded = true
