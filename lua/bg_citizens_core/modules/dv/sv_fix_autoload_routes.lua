1b74f17101031060ec8b8341d10fc89d4cd931ff
Shark-vil <shark_vil@mail.ru>
2021-03-05 01:12:45 +0300

    fix dv autoloader post cleanup map

--- lua/bg_citizens_core/modules/dv/sv_fix_autoload_routes.lua
+++ lua/bg_citizens_core/modules/dv/sv_fix_autoload_routes.lua
@@ -1,3 +1,9 @@
 hook.Add('PostCleanupMap', 'BGN_DV_FixRoutesAutoLoadPostCleanupMap', function()
-   hook.Run("InitPostEntity", "Decent Vehicle: Load waypoints")
+   local HOOKS_DATA = hook.GetTable()
+   if HOOKS_DATA['InitPostEntity'] then
+      if HOOKS_DATA['InitPostEntity']['Decent Vehicle: Load waypoints'] then
+         local func = HOOKS_DATA['InitPostEntity']['Decent Vehicle: Load waypoints']
+         func()
+      end
+   end
 end)
