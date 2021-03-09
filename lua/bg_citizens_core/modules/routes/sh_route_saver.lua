d4f4011663dae1b2b8f0e85f5eac496006b4eb37
Shark-vil <shark_vil@mail.ru>
2021-03-09 09:26:46 +0300

    Added warning before saving empty map

--- lua/bg_citizens_core/modules/routes/sh_route_saver.lua
+++ lua/bg_citizens_core/modules/routes/sh_route_saver.lua
@@ -124,13 +124,69 @@ else
 					end
 				end
 			end
+
+			snet.InvokeBigData('bgn_save_routes', nil, { 
+				from_json = from_json, 
+				data = save_table
+			}, nil, 'BgnLoadPoints', 'Loading points...')
+	
+			notification.AddLegacy("[For admin] The new mesh has been sent to the server.", NOTIFY_GENERIC, 4)
+		else
+			local MainMenu = vgui.Create("DFrame")
+			MainMenu:SetPos(ScrW()/2 - 500/2, ScrH()/2 - 230/2)
+			MainMenu:SetSize(500, 230)
+			MainMenu:SetTitle("Background NPCs - Warning!")
+			MainMenu:SetDraggable(true)
+			MainMenu:MakePopup()
+			MainMenu.Paint = function(self, w, h)
+				draw.RoundedBox(8, 0, 0, w, h, Color(0, 0, 0, 250))
+			end
+
+			local maleImage = vgui.Create("DImage", MainMenu)
+			maleImage:SetPos(10, 90)
+			maleImage:SetSize(150, 150)
+			maleImage:SetImage("background_npcs/vgui/missing_slib.png")
+
+			local MainMenu_Label = vgui.Create("DLabel", MainMenu)
+			MainMenu_Label:SetPos(170, 20)
+			MainMenu_Label:SetSize(350, 150)
+			if GetConVar('cl_language'):GetString() == 'russian' then
+				MainMenu_Label:SetText([[ВНИМАНИЕ!
+
+				Вы собирайтесь сохранить пустую карту передвижения.
+				Вы окончательно замените старый файл на сервере.
+				Нажмите "Save" если уверены в своих действиях.
+				]])
+			else
+				MainMenu_Label:SetText([[ATTENTION!
+
+				Are you going to keep a empty movement map.
+				You will permanently replace the old file on the server.
+				Click "Save" if you are sure of your actions.
+				]])
+			end
+
+			local ButtonYes = vgui.Create("DButton", MainMenu)
+			ButtonYes:SetText("Save")
+			ButtonYes:SetPos(170, 170)
+			ButtonYes:SetSize(155, 30)
+			ButtonYes.DoClick = function()
+				snet.InvokeBigData('bgn_save_routes', nil, { 
+					from_json = from_json, 
+					data = save_table
+				}, nil, 'BgnLoadPoints', 'Loading points...')
+		
+				notification.AddLegacy("[For admin] The new mesh has been sent to the server.", NOTIFY_GENERIC, 4)
+				MainMenu:Close()
+			end
+
+			local ButtonNo = vgui.Create("DButton", MainMenu)
+			ButtonNo:SetText("Cancel")
+			ButtonNo:SetPos(350, 170)
+			ButtonNo:SetSize(100, 30)
+			ButtonNo.DoClick = function()
+				MainMenu:Close()
+			end
 		end
-
-		snet.InvokeBigData('bgn_save_routes', nil, { 
-			from_json = from_json, 
-			data = save_table
-		}, nil, 'BgnLoadPoints', 'Loading points...')
-
-		notification.AddLegacy("[For admin] The new mesh has been sent to the server.", NOTIFY_GENERIC, 4)
 	end, nil, 'Saves movement points (Only if the player has a tool weapon!)')
 end
