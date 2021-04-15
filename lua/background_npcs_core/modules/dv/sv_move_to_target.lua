4b0750fa52b8a08108a9c4abf7707df77f133224
Shark-vil <shark_vil@mail.ru>
2021-04-15 07:44:24 +0300

    Added attack from vehicle

--- lua/background_npcs_core/modules/dv/sv_move_to_target.lua
+++ lua/background_npcs_core/modules/dv/sv_move_to_target.lua
@@ -16,12 +16,53 @@ timer.Create('BGN_DVCars_ExitAnVhicleIfLowerDistance', 0.5, 0, function()
                if actor and actor:IsAlive() and actor:InDangerState() and not actor:HasState('fear') then
                   local enemy = actor:GetNearEnemy()
                   if IsValid(enemy) then
-                     local delay = vehicle_provider.actorsExitDelay or 0
-                     local isTrueDistance = enemy:GetPos():DistToSqr(vehiclePosition) <= 640000
-                     if delay < CurTime() and (isTrueDistance or not driver) then
-                        actor:ExitVehicle()
-                        vehicle_provider.actorsExitDelay = CurTime() + 0.5
+                     local dist = enemy:GetPos():DistToSqr(vehiclePosition)
+                  
+                     if enemy:IsPlayer() and enemy:InVehicle() and driver.weapon then
+                        local isTrueDistance = dist <= 1000000
+                        if isTrueDistance then
+                           vehicle_provider.bulletFireDelay = vehicle_provider.bulletFireDelay or 0
+                           vehicle_provider.bulletIsFire = vehicle_provider.bulletIsFire or false
+
+                           if vehicle_provider.bulletFireDelay < CurTime() then
+                              vehicle_provider.bulletIsFire = not vehicle_provider.bulletIsFire
+                           end
+
+                           if vehicle_provider.bulletIsFire then
+                              local limit = math.random(3, 5)
+                              local delay = math.random(0.2, 0.4)
+                              local shoot_vector = enemy:GetPos() - decentvehicle:GetPos()
+				                  shoot_vector:Normalize()
+
+                              decentvehicle:slibCreateTimer('dv_fire_enemy', delay, limit, function(decentvehicle)
+                                 local bullet = {}
+                                 bullet.Num = 1
+                                 bullet.Src = decentvehicle:GetPos() + decentvehicle:GetForward() * 100
+                                    + decentvehicle:GetUp() * 50
+                                 bullet.Damage = math.random(4, 8)
+                                 bullet.Force = 50
+                                 bullet.Tracer = 1
+                                 bullet.Spread = Vector(5 / 90, 5 / 90, 0)
+                                 bullet.Dir = shoot_vector
+                                 bullet.IgnoreEntity = vehicle
+
+                                 decentvehicle:FireBullets(bullet)
+                                 vehicle:EmitSound('weapons/smg1/smg1_fire1.wav', 100, 100)
+                              end)
+
+                              vehicle_provider.bulletIsFire = false
+                              vehicle_provider.bulletFireDelay = CurTime() + limit + delay + 3
+                           end
+                        end
+                     else
+                        local isTrueDistance = dist <= 640000
+                        local delay = vehicle_provider.actorsExitDelay or 0
+                        if delay < CurTime() and (isTrueDistance or not driver) then
+                           actor:ExitVehicle()
+                           vehicle_provider.actorsExitDelay = CurTime() + 0.5
+                        end
                      end
+
                      actor:WalkToTarget(enemy)
                   end
 
