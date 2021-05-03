c7390d5324c5b36533244d990092ea0457fb5d5b
Shark-vil <shark_vil@mail.ru>
2021-05-04 01:25:58 +0300

    Improved logic optimization. Creation of optimization service changed to implementation using a module.

--- lua/background_npcs_core/modules/sv_run_logic_optimization.lua
+++ lua/background_npcs_core/modules/sv_run_logic_optimization.lua
@@ -1,85 +1,93 @@
-local no_think_actors = {}
+local bgn_enable = GetConVar('bgn_enable'):GetBool()
+local bgn_disable_logic_radius = GetConVar('bgn_disable_logic_radius'):GetFloat() ^ 2
 
-local function logic_optimization()
-   if not GetConVar('bgn_enable'):GetBool() then return end
-   
-	local bgn_disable_logic_radius = GetConVar('bgn_disable_logic_radius'):GetFloat() ^ 2
-   if bgn_disable_logic_radius <= 0 then return end
+cvars.AddChangeCallback('bgn_disable_logic_radius', function(_, _, new_value)
+   if tonumber(new_value) > 0 then return end
+
+   local actors = bgNPC:GetAll()
+   for i = 1, #actors do
+      local actor = actors[i]
+      if actor and actor:IsAlive() and not actor:InVehicle() then
+         actor:GetNPC():RemoveEFlags(EFL_NO_THINK_FUNCTION)
+      end
+   end
+end)
+
+cvars.AddChangeCallback('bgn_enable', function(_, _, new_value)
+   bgn_enable = tobool(new_value)
+end)
+
+cvars.AddChangeCallback('bgn_disable_logic_radius', function(_, _, new_value)
+   bgn_disable_logic_radius = tonumber(new_value) ^ 2
+end)
+
+local max_pass = 5
+local function async_method(yield)
+   if not bgn_enable or bgn_disable_logic_radius <= 0 then return end
 
    local actors = bgNPC:GetAll()
    local players = player.GetAll()
    local pass = 0
-   local max_pass = 5
 
-   coroutine.yield()
+   yield()
 
-	for i = 1, #actors do
+   for i = 1, #actors do
       local actor = actors[i]
       if actor and not actor:InVehicle() then
          local npc = actor:GetNPC()
-         if npc ~= NULL then
+         if IsValid(npc) then
             local npc_pos = npc:GetPos()
             local max_dist = nil
-            local addIgnoreFlag = true
+            local is_adding_no_think_flag = true
 
-            for p = 1, #players do
-               local ply = players[p]
-               if ply and ply ~= NULL then               
+            for k = 1, #players do
+               local ply = players[k]
+               if IsValid(ply) then
                   local dist = npc_pos:DistToSqr(ply:GetPos())
 
-                  if not max_dist or dist < max_dist then
-                     max_dist = dist
-                  end
+                  if not max_dist or dist < max_dist then max_dist = dist end
 
-                  if dist <= bgn_disable_logic_radius or bgNPC:PlayerIsViewVector(ply, npc_pos) then
-                     addIgnoreFlag = false
+                  if dist <= bgn_disable_logic_radius or ply:slibIsViewVector(npc_pos) then
+                     is_adding_no_think_flag = false
                      break
                   end
                end
             end
 
-            local bgnIgmoreFlagEnabled = npc.bgnIgmoreFlagEnabled or false
+            local no_think_state = npc:slibGetLocalVar('bgn_optimize_no_think_enable', false)
+            local no_think_enable = no_think_state
 
-            if addIgnoreFlag then
-               local delay = npc.bgnIgmoreFlagSwitchDelay or 0
+            if is_adding_no_think_flag then
+               local delay = npc:slibGetLocalVar('bgn_optimize_no_think_delay', 0)
                local time = CurTime()
                if delay < time then
-                  bgnIgmoreFlagEnabled = not npc.bgnIgmoreFlagEnabled
+                  no_think_enable = not no_think_enable
 
-                  if bgnIgmoreFlagEnabled and max_dist >= 1000000 then
+                  if no_think_enable and max_dist >= 1000000 then
                      delay = time + 3
                   else
                      delay = time + 1
                   end
                end
-               npc.bgnIgmoreFlagSwitchDelay = delay
-            elseif bgnIgmoreFlagEnabled then
-               bgnIgmoreFlagEnabled = false
+               npc:slibSetLocalVar('bgn_optimize_no_think_delay', delay)
+            elseif no_think_enable then
+               no_think_enable = false
             end
 
-            if npc.bgnIgmoreFlagEnabled ~= bgnIgmoreFlagEnabled then
-               if bgnIgmoreFlagEnabled then
+            if no_think_state ~= no_think_enable then
+               if no_think_enable then
                   npc:AddEFlags(EFL_NO_THINK_FUNCTION)
                else
                   npc:RemoveEFlags(EFL_NO_THINK_FUNCTION)
                end
-               npc.bgnIgmoreFlagEnabled = bgnIgmoreFlagEnabled
+               npc:slibSetLocalVar('bgn_optimize_no_think_enable', no_think_enable)
             end
 
             pass = pass + 1
-            if pass == max_pass then
-               pass = 0
-               coroutine.yield()
-            end
+
+            if pass == max_pass then pass = 0; yield() end
          end
       end
    end
 end
-
-local thread
-hook.Add('Think', 'BGN_ActorsLogicOptimization', function()
-   if not thread or not coroutine.resume(thread) then
-		thread = coroutine.create(logic_optimization)
-		coroutine.resume(thread)
-	end
-end)
+async.Add('bgn_server_logic_optimization', async_method)
