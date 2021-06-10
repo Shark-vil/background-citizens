b1484393c2b2f671b89658cebcd25a7778bd2504
Shark-vil <shark_vil@mail.ru>
2021-06-10 06:07:31 +0300

    fix tab

--- lua/background_npcs_core/tests/unit/sv_unit_test_exist_nodes.lua
+++ lua/background_npcs_core/tests/unit/sv_unit_test_exist_nodes.lua
@@ -1,8 +1,8 @@
 snet.Callback('bgn_sv_unit_test_exist_nodes', function(ply)
-   local nodes = bgNPC.LoadRoutes()
-   if #nodes ~= 0 then
-      ply:ConCommand('bgn_unit_test_add_result "The movement mesh is not empty" "yes"')
-   else
-      ply:ConCommand('bgn_unit_test_add_result "The movement mesh is not empty" "no"')
-   end
-end).Protect().Register()
+	local nodes = bgNPC.LoadRoutes()
+	if #nodes ~= 0 then
+		ply:ConCommand('bgn_unit_test_add_result "The movement mesh is not empty" "yes"')
+	else
+		ply:ConCommand('bgn_unit_test_add_result "The movement mesh is not empty" "no"')
+	end
+end).Protect()
