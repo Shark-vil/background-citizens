4b55a82c9ff09de2acdaa7f1cd9a15f71c011be1
Shark-vil <shark_vil@mail.ru>
2021-06-11 20:08:46 +0300

    Fix autoload to DV waypoints

--- lua/background_npcs_core/modules/dv/sv_fix_autoload_routes.lua
+++ lua/background_npcs_core/modules/dv/sv_fix_autoload_routes.lua
@@ -1,4 +1,12 @@
-hook.Add('PostCleanupMap', 'BGN_DV_FixRoutesAutoLoadPostCleanupMap', function()
+local function init_dv_waypoints()
+   if not DecentVehicleDestination then return end
+
    local hook_function = hook.Get('InitPostEntity', 'Decent Vehicle: Load waypoints')
-   if hook_function then hook_function() end
-end)
+   if hook_function then
+      hook_function()
+   else
+      MsgN('[Background NPCs] Failed to call the function of loading the waypoints of DV')
+   end
+end
+hook.Add('PostCleanupMap', 'BGN_DV_FixRoutesAutoLoadPostCleanupMap', init_dv_waypoints)
+hook.Add('InitPostEntity', 'BGN_DV_RoutesAutoLoadAfterInitEntities', init_dv_waypoints)
