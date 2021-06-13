31ec6754425a928ddbd558f0c831faa93af856bb
Shark-vil <shark_vil@mail.ru>
2021-06-13 19:34:08 +0300

    Add sound and effect when arresting

--- lua/background_npcs_core/modules/darkrp/sv_player_arrest.lua
+++ lua/background_npcs_core/modules/darkrp/sv_player_arrest.lua
@@ -1,4 +1,6 @@
 hook.Add("BGN_PlayerArrest", "BGN_DarkRP_PlayerArrest", function(ply, actor)
    if engine.ActiveGamemode() ~= 'darkrp' then return end
    ply:arrest(nil, actor:GetNPC())
+   ply:EmitSound('background_npcs/handcuffs_sound1.mp3')
+   ply:ScreenFade(SCREENFADE.IN, Color(0, 0, 0, 255), 1, 3)
 end)
