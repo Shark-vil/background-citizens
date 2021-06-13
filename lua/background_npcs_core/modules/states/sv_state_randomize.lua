34c2aed72d28ca8b5799f1175b0d7c13228a2bdf
Shark-vil <shark_vil@mail.ru>
2021-06-13 21:57:21 +0300

    Added value for delay state

--- lua/background_npcs_core/modules/states/sv_state_randomize.lua
+++ lua/background_npcs_core/modules/states/sv_state_randomize.lua
@@ -1,6 +1,6 @@
 timer.Create('BGN_Timer_StateRandomize', bgNPC.cfg.RandomStateAssignmentPeriod, 0, function()
    for _, actor in ipairs(bgNPC:GetAll()) do
-      if actor:IsAlive() and not actor:IsStateLock() 
+      if actor:IsAlive() and not actor:IsStateLock() and actor.state_delay < CurTime()
          and actor:EnemiesCount() == 0 and actor:TargetsCount() == 0
       then
          actor:RandomState()
