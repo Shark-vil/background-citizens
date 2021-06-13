4c220c6578ee8735f65b6a4e18619103815a7a69
Shark-vil <shark_vil@mail.ru>
2021-06-13 19:53:23 +0300

    Optimize parsing of sync data

--- lua/background_npcs_core/modules/synchronization/sv_cync.lua
+++ lua/background_npcs_core/modules/synchronization/sv_cync.lua
@@ -10,43 +10,57 @@ timer.Create('BGN_SynchronizationService', 1.5, 0, function()
 
 		for i = 1, #actors do
 			local actor = actors[i]
+			local sync_players = {}
+
 			if actor:IsAlive() then
 				local npc = actor:GetNPC()
 				local pos = npc:GetPos()
 				for k = 1, #players do
 					local ply = players[k]
-					if ply:GetPos():DistToSqr(pos) <= sync_distance and ply:slibIsViewVector(pos, sync_radius) then
-						sync_list[actor] = sync_list[actor] or {}
-						array.insert(sync_list[actor], ply)
+					if ply:GetPos():DistToSqr(pos) <= sync_distance and ply:slibIsViewVector(pos) then
+						array.insert(sync_players, ply)
 					end
 				end
+			end
+
+			if #sync_players ~= 0 then
+				array.insert(sync_list, {
+					actor = actor,
+					players = sync_players
+				})
 			end
 		end
 	end
 
-	for actor, players in pairs(sync_list) do
-		local sync_data = {
-			state = actor:GetState(),
-			enemies = actor.enemies,
-			targets = actor.targets,
-			anim_name = actor.anim_name,
-			is_animated = actor.is_animated,
-			state_lock = actor.state_lock,
-			reaction = actor.reaction,
-			npc_schedule = actor.npc_schedule,
-			npc_state = actor.npc_state,
-		}
+	for i = 1, #sync_list do
+		local sync_list_value = sync_list[i]
+		local actor = sync_list_value.actor
 
-		local sync_hash = slib.GetHashSumm(sync_data)
+		if actor and actor:IsAlive() then
+			local players = sync_list_value.players
+			local sync_data = {
+				state = actor:GetState(),
+				enemies = actor.enemies,
+				targets = actor.targets,
+				anim_name = actor.anim_name,
+				is_animated = actor.is_animated,
+				state_lock = actor.state_lock,
+				reaction = actor.reaction,
+				npc_schedule = actor.npc_schedule,
+				npc_state = actor.npc_state,
+			}
 
-		for i = 1, #players do
-			local ply = players[i]
-			if IsValid(ply) and actor:IsAlive() then
-				local old_sync_hash = actor.sync_players_hash[ply] or -1
-				if sync_hash == old_sync_hash then return end
+			local sync_hash = slib.GetHashSumm(sync_data)
 
-				snet.Invoke('bgn_actor_sync_data', ply, actor.uid, sync_data)
-				actor.sync_players_hash[ply] = sync_hash
+			for k = 1, #players do
+				local ply = players[k]
+				if ply and ply.snet_ready then
+					local old_sync_hash = actor.sync_players_hash[ply] or -1
+					if sync_hash ~= old_sync_hash then
+						snet.Invoke('bgn_actor_sync_data', ply, actor.uid, sync_data)
+						actor.sync_players_hash[ply] = sync_hash
+					end
+				end
 			end
 		end
 	end
