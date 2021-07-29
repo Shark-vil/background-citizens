1ecaa0eda3fc7497fcfe5e47ef3feb194177c4b6
Shark-vil <shark_vil@mail.ru>
2021-07-29 23:30:47 +0300

    Update "sv_impingement" to a new state template

--- lua/background_npcs_core/states/sv_impingement.lua
+++ lua/background_npcs_core/states/sv_impingement.lua
@@ -72,5 +72,8 @@ bgNPC:SetStateAction('impingement', {
 
 			data.delay = CurTime() + 3
 		end
+	end,
+	not_stop = function(actor, state, data, new_state, new_data)
+		return actor:EnemiesCount() > 0 and not actor:HasDangerState(new_state)
 	end
 })
