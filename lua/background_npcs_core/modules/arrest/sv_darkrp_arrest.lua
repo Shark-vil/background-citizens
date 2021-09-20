93fcfec38f27d37be6cde1a4b3f38e9f95ebac73
Shark-vil <shark_vil@mail.ru>
2021-09-21 01:29:30 +0300

    Move sandbox & darkrp arrest module another folder

--- lua/background_npcs_core/modules/arrest/sv_darkrp_arrest.lua
+++ lua/background_npcs_core/modules/arrest/sv_darkrp_arrest.lua
@@ -1,4 +1,4 @@
-hook.Add("BGN_PlayerArrest", "BGN_DarkRP_PlayerArrest", function(ply, actor)
+hook.Add('BGN_PlayerArrest', 'BGN_DarkRP_PlayerArrest', function(ply, actor)
    if engine.ActiveGamemode() ~= 'darkrp' then return end
    ply:arrest(nil, actor:GetNPC())
    ply:EmitSound('background_npcs/handcuffs_sound1.mp3')
