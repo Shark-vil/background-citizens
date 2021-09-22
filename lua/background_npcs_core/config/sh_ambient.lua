89bf04641e95c8f3d1fb309dee1bb01a1702b29e
Shark-vil <shark_vil@mail.ru>
2021-09-22 17:02:23 +0300

    Make background sounds expandable

---
+++ lua/background_npcs_core/config/sh_ambient.lua
@@ -0,0 +1,5 @@
+bgNPC.cfg.ambient = {
+	{ count = 1, sound = 'background_npcs/ambient/bgn_ambient_3.wav' },
+	{ count = 3, sound = 'background_npcs/ambient/bgn_ambient_2.wav' },
+	{ count = 6, volume = 0.4, sound = 'background_npcs/ambient/bgn_ambient_1.wav' },
+}
--- lua/background_npcs_core/modules/ambient/cl_ambient_sound.lua
+++ lua/background_npcs_core/modules/ambient/cl_ambient_sound.lua
@@ -1,24 +1,37 @@
-local oldLevel
-local currentLevel
+local currentAmbient
 local currentSound
 
-local function SetAmbient(level)
-	if level == currentLevel then return end
-	oldLevel = currentLevel
-	currentLevel = level
+local function SetAmbient(ambient_value)
+	if not ambient_value then
+		if currentSound and currentAmbient and currentSound:IsPlaying() then
+			if timer.Exists('BGN_SetNewAmbientSoundAfterFade') then
+				timer.Remove('BGN_SetNewAmbientSoundAfterFade')
+			end
 
-	if currentLevel == 0 then
-		if currentSound ~= nil and currentSound:IsPlaying() then
-			timer.Remove('BGN_SetNewAmbientSoundAfterFade')
-			currentSound:Stop()
-			bgNPC:Log('Stop ambient - ' .. 'background_npcs/ambient/bgn_ambient_' .. oldLevel .. '.wav')
-			currentSound = nil
+			if timer.Exists('BGN_SetNewAmbientSoundAfterFadeChangeVolume') then
+				timer.Remove('BGN_SetNewAmbientSoundAfterFadeChangeVolume')
+			end
+
+			currentSound:FadeOut(2)
+
+			timer.Create('BGN_AmbientSoundStopFade', 2.1, 1, function()
+				if not currentSound then return end
+				currentSound:Stop()
+				currentSound = nil
+			end)
+
+			bgNPC:Log('Stop ambient - ' .. currentAmbient)
 		end
 
+		currentAmbient = nil
 		return
 	end
 
-	local sound_name = 'background_npcs/ambient/bgn_ambient_' .. level .. '.wav'
+	if ambient_value.sound == currentAmbient then return end
+	currentAmbient = ambient_value.sound
+
+	local sound_name = currentAmbient
+	local sound_volume = ambient_value.volume ~= nil and ambient_value.volume or 1
 	local fade_time = 2
 
 	if currentSound ~= nil and currentSound:IsPlaying() then
@@ -29,30 +42,37 @@ local function SetAmbient(level)
 
 	timer.Create('BGN_SetNewAmbientSoundAfterFade', fade_time + 0.1, 1, function()
 		if currentSound ~= nil then
-				currentSound:Stop()
-				bgNPC:Log('Stop ambient - ' .. 'background_npcs/ambient/bgn_ambient_' .. oldLevel .. '.wav')
-		end
-
-		local volume = 1
-
-		if level == 1 then
-			volume = 0.4
+			currentSound:Stop()
+			bgNPC:Log('Stop ambient - ' .. tostring(currentAmbient))
 		end
 
 		currentSound = CreateSound(game.GetWorld(), sound_name)
 		currentSound:SetSoundLevel(0)
-		currentSound:PlayEx(volume, 100)
+		currentSound:PlayEx(0, 100)
+		timer.Create('BGN_SetNewAmbientSoundAfterFadeChangeVolume', 0.01, 0, function()
+			local current_volume = currentSound:GetVolume()
+
+			if not currentSound or current_volume == sound_volume then
+				timer.Remove('BGN_SetNewAmbientSoundAfterFadeChangeVolume')
+				return
+			end
+
+			current_volume = current_volume + 0.1
+			if current_volume > sound_volume then
+				current_volume = sound_volume
+			end
+
+			bgNPC:Log('Ambient volume - ' .. current_volume)
+
+			currentSound:ChangeVolume(current_volume)
+		end)
 		bgNPC:Log('Play ambient - ' .. sound_name)
 	end)
 end
 
-local min_ambient_1 = 6
-local min_ambient_2 = 3
-
--- 3 automatically
 timer.Create('BGN_SetAmbientSound', 2, 0, function()
 	if not GetConVar('bgn_cl_ambient_sound'):GetBool() then
-		SetAmbient(0)
+		SetAmbient()
 		return
 	end
 
@@ -71,15 +91,18 @@ timer.Create('BGN_SetAmbientSound', 2, 0, function()
 	end
 
 	if count == 0 then
-		SetAmbient(0)
+		SetAmbient()
 		return
 	end
 
-	if count >= min_ambient_1 then
-		SetAmbient(1)
-	elseif count >= min_ambient_2 then
-		SetAmbient(2)
-	else
-		SetAmbient(3)
+	table.sort(bgNPC.cfg.ambient, function(a, b)
+		return a.count > b.count
+	end)
+
+	for _, v in ipairs(bgNPC.cfg.ambient) do
+		if count >= v.count then
+			SetAmbient(v)
+			break
+		end
 	end
 end)
