bb342666efbc9371e1d7b64d1c9feb6511544a8c
Shark-vil <shark_vil@mail.ru>
2021-09-23 22:59:50 +0300

    More options description

--- lua/background_npcs_core/tool_options/cl_general_settings.lua
+++ lua/background_npcs_core/tool_options/cl_general_settings.lua
@@ -1,40 +1,46 @@
 local function TOOL_MENU(Panel)
 	Panel:AddControl('CheckBox', {
 		Label = '#bgn.settings.general.bgn_enable',
-		Command = 'bgn_enable' 
+		Command = 'bgn_enable'
 	}); Panel:AddControl('Label', {
 		Text = '#bgn.settings.general.bgn_enable.description'
 	})
 
 	Panel:AddControl('CheckBox', {
 		Label = '#bgn.settings.general.bgn_ignore_another_npc',
-		Command = 'bgn_ignore_another_npc' 
+		Command = 'bgn_ignore_another_npc'
 	}); Panel:AddControl('Label', {
 		Text = '#bgn.settings.general.bgn_ignore_another_npc.description'
 	})
 
-	Panel:AddControl("Button", {
-		["Label"] = "#bgn.settings.general.cl_citizens_load_route",
-		["Command"] = "cl_citizens_load_route ",
+	Panel:AddControl('Button', {
+		['Label'] = '#bgn.settings.general.cl_citizens_load_route',
+		['Command'] = 'cl_citizens_load_route ',
 	}); Panel:AddControl('Label', {
 		Text = '#bgn.settings.general.cl_citizens_load_route.description'
 	})
 
 	Panel:AddControl('CheckBox', {
 		Label = '#bgn.settings.general.bgn_debug',
-		Command = 'bgn_debug' 
+		Command = 'bgn_debug'
 	}); Panel:AddControl('Label', {
 		Text = '#bgn.settings.general.bgn_debug.description'
 	})
 
-	Panel:AddControl("Button", {
-		["Label"] = "#bgn.settings.general.bgn_updateinfo",
-		["Command"] = "bgn_updateinfo",
+	Panel:AddControl('Button', {
+		['Label'] = '#bgn.settings.general.bgn_updateinfo',
+		['Command'] = 'bgn_updateinfo',
+	}); Panel:AddControl('Label', {
+		Text = '#bgn.settings.general.bgn_updateinfo.description'
 	})
 
-	Panel:AddControl("Button", {
-		["Label"] = "#bgn.settings.general.bgn_reset_cvars_to_factory_settings",
-		["Command"] = "bgn_reset_cvars_to_factory_settings",
+	Panel:AddControl('Button', {
+		['Label'] = '#bgn.settings.general.bgn_reset_cvars_to_factory_settings',
+		['Command'] = 'bgn_reset_cvars_to_factory_settings',
+	}); Panel:AddControl('Label', {
+		Text = '#bgn.settings.general.bgn_reset_cvars_to_factory_settings.description'
+	})
+
 	Panel:AddControl('Button', {
 		['Label'] = '#bgn.settings.general.bgn_remove_routes',
 		['Command'] = 'cl_citizens_remove_route yes',
@@ -43,7 +49,7 @@ local function TOOL_MENU(Panel)
 	})
 end
 
-hook.Add("PopulateToolMenu", "BGN_TOOL_CreateMenu_GeneralSettings", function()
-	spawnmenu.AddToolMenuOption("Options", "Background NPCs", "BGN_General_Settings", 
-		"#bgn.settings.general_title", "", "", TOOL_MENU)
+hook.Add('PopulateToolMenu', 'BGN_TOOL_CreateMenu_GeneralSettings', function()
+	spawnmenu.AddToolMenuOption('Options', 'Background NPCs', 'BGN_General_Settings', 
+		'#bgn.settings.general_title', '', '', TOOL_MENU)
 end)
