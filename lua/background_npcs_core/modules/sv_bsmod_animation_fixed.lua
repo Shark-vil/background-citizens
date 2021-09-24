a6c9203a450557517a6718dbf11dc5ed931c9a0c
Shark-vil <shark_vil@mail.ru>
2021-09-24 03:30:49 +0300

    fixed bsmod damage animation

---
+++ lua/background_npcs_core/modules/sv_bsmod_animation_fixed.lua
@@ -0,0 +1,20 @@
+local original_hook_function
+
+local function init_bsmod_animations()
+	original_hook_function = hook.Get('EntityTakeDamage', 'DAModuleDamage')
+	if not original_hook_function then return end
+
+	hook.Add('EntityTakeDamage', 'DAModuleDamage', function(target, dmginfo)
+		if target and IsValid(target) then
+			local actor = bgNPC:GetActor(target)
+			if actor and ( actor.bsmod_damage_animation_disable
+				or actor:GetData().bsmod_damage_animation_disable
+			) then
+				return
+			end
+		end
+
+		return original_hook_function(target, dmginfo)
+	end)
+end
+hook.Add('InitPostEntity', 'BGN_BSMOD_DamageAnimationFixedCaller', init_bsmod_animations)
