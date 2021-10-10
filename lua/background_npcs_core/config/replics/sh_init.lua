b78b95cf411dfd340633fea8210c93fc65dcf7db
Shark-vil <shark_vil@mail.ru>
2021-10-10 13:49:38 +0300

    Move replics load to hook

--- lua/background_npcs_core/config/replics/sh_init.lua
+++ lua/background_npcs_core/config/replics/sh_init.lua
@@ -5,21 +5,27 @@ if SERVER then
 	AddCSLuaFile('language/sh_ru.lua')
 end
 
-local full_path_to_language_dir = 'background_npcs_core/config/replics/'
-local default_file_path = 'language/sh_' .. GetConVar('bgn_module_replics_language'):GetString() .. '.lua'
+include('language/sh_en.lua')
 
-if not file.Exists(full_path_to_language_dir .. default_file_path, 'LUA') then
-	default_file_path = 'language/sh_' .. GetConVar('cl_language'):GetString() .. '.lua'
-end
+hook.Add('BGN_PostGamemodeLoaded', 'BGN_LoadConfig_Replics_SH_Init', function()
+	bgNPC:ClearActorsConfig()
 
-if not file.Exists(full_path_to_language_dir .. default_file_path, 'LUA') then
-	default_file_path = 'language/sh_en.lua'
-end
+	local full_path_to_language_dir = 'background_npcs_core/config/replics/'
+	local default_file_path = 'language/sh_' .. GetConVar('bgn_module_replics_language'):GetString() .. '.lua'
 
-cvars.AddChangeCallback('bgn_module_replics_language', function(_, _, lang)
-	if file.Exists(full_path_to_language_dir .. 'language/sh_' .. lang .. '.lua', 'LUA') then
-		include('language/sh_' .. lang .. '.lua')
+	if not file.Exists(full_path_to_language_dir .. default_file_path, 'LUA') then
+		default_file_path = 'language/sh_' .. GetConVar('cl_language'):GetString() .. '.lua'
 	end
-end)
 
-include(default_file_path)
+	if not file.Exists(full_path_to_language_dir .. default_file_path, 'LUA') then
+		default_file_path = 'language/sh_en.lua'
+	end
+
+	cvars.AddChangeCallback('bgn_module_replics_language', function(_, _, lang)
+		if file.Exists(full_path_to_language_dir .. 'language/sh_' .. lang .. '.lua', 'LUA') then
+			include('language/sh_' .. lang .. '.lua')
+		end
+	end)
+
+	include(default_file_path)
+end)
