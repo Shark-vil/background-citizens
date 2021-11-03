392411944fe6efda6ea2f52e168e79225cea51a0
Shark-vil <shark_vil@mail.ru>
2021-11-04 02:42:44 +0300

    Admin command for viewing the config (For debugging)

--- lua/background_npcs_core/commands/sh_cmd_config.lua
+++ lua/background_npcs_core/commands/sh_cmd_config.lua
@@ -2,3 +2,16 @@ scommand.Create('bgn_config_reload').OnShared(function()
 	bgNPC:ClearActorsConfig()
 end).Access( { isAdmin = true } ).Broadcast().Register()
 
+scommand.Create('bgn_config_view').OnShared(function(_, _, args)
+	local actor_type = args[1]
+	if not bgNPC.cfg.npcs_template[actor_type] then return end
+	PrintTable(bgNPC:GetActorConfig(actor_type))
+end).AutoComplete(function(cmd)
+	local actors_type_list = {}
+
+	for actor_type, _ in pairs(bgNPC.cfg.npcs_template) do
+		table.insert(actors_type_list, cmd .. ' "' .. actor_type .. '"')
+	end
+
+	return actors_type_list
+end).Access( { isAdmin = true } ).Register()
