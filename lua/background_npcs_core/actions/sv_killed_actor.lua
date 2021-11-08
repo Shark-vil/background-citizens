c84b64044b28e7ffc186a5ea17d2c44a4b545f28
Shark-vil <shark_vil@mail.ru>
2021-11-08 18:31:02 +0300

    Count killed actors who are not NPCs

--- lua/background_npcs_core/actions/sv_killed_actor.lua
+++ lua/background_npcs_core/actions/sv_killed_actor.lua
@@ -1,13 +1,49 @@
-hook.Add('OnNPCKilled', 'BGN_OnKilledActor', function(npc, attacker, inflictor)
+local EntityRemovedLock = false
+
+local function Call_BGN_OnKilledActor(actor, npc, attacker)
+	bgNPC:Log(tostring(attacker) .. ' killed ' .. actor:GetName(), 'OnNPCKilled')
+
+	bgNPC:AddKillingStatistic(attacker, actor)
+	bgNPC:AddWantedKillingStatistic(attacker, actor)
+
+	hook.Run('BGN_OnKilledActor', actor, attacker)
+end
+
+hook.Add('OnNPCKilled', 'BGN_OnKilledActor', function(npc, attacker)
 	local actor = bgNPC:GetActor(npc)
-	if actor then
-		bgNPC:Log(tostring(attacker) .. ' killed ' .. actor:GetName(), 'OnNPCKilled')
+	if not actor then return end
 
-		bgNPC:AddKillingStatistic(attacker, actor)
-		bgNPC:AddWantedKillingStatistic(attacker, actor)
+	actor.OnNPCKilled = true
+	Call_BGN_OnKilledActor(actor, npc, attacker)
+end)
 
-		hook.Run('BGN_OnKilledActor', actor, attacker)
+hook.Add('EntityRemoved', 'BGN_OnKilledActorByRemoved', function(npc)
+	local actor = bgNPC:GetActor(npc)
+	if not actor or actor.OnNPCKilled or not actor.EntityTakeDamageInfo or EntityRemovedLock then
+		return
 	end
+
+	local attacker = actor.EntityTakeDamageInfo.dmginfo:GetAttacker()
+	if not IsValid(attacker) then return end
+
+	Call_BGN_OnKilledActor(actor, npc, attacker)
+end)
+
+hook.Add('EntityTakeDamage', 'BGN_OnKilledActorByRemoved', function(target, dmginfo)
+	local actor = bgNPC:GetActor(target)
+	if not actor then return end
+
+	actor.EntityTakeDamageInfo = actor.EntityTakeDamageInfo or {}
+	actor.EntityTakeDamageInfo.target = target
+	actor.EntityTakeDamageInfo.dmginfo = dmginfo
+end)
+
+hook.Add('PreCleanupMap', 'BGN_OnKilledActorLock', function(npc)
+	EntityRemovedLock = true
+end)
+
+hook.Add('PostCleanupMap', 'BGN_OnKilledActorUnlock', function(npc)
+	EntityRemovedLock = false
 end)
 
 hook.Add('BGN_OnKilledActor', 'BGN_DelayActorRespawn', function(actor)
