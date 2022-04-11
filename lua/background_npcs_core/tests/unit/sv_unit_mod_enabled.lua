869fcd3bfab934e492a4762ce79129910f26c8d8
Shark-vil <shark_vil@mail.ru>
2022-04-11 04:34:32 +0300

    Delete all unit tests

--- lua/background_npcs_core/tests/unit/sv_unit_mod_enabled.lua
+++
@@ -1,7 +0,0 @@
-snet.Callback('bgn_sv_unit_test_mod_enabled', function(ply)
-	if GetConVar('bgn_enable'):GetBool() then
-		ply:ConCommand('bgn_unit_test_add_result "Addon functionality is active" "yes"')
-	else
-		ply:ConCommand('bgn_unit_test_add_result "Addon functionality is active" "no"')
-	end
-end).Protect()
