b3fc65f6186c5d8fb91659d369e80b581ecdc622
Shark-vil <shark_vil@mail.ru>
2022-05-22 08:23:01 +0300

    Change format options language include

--- lua/background_npcs_core/tool_options/lang/cl_ru.lua
+++ lua/background_npcs_core/tool_options/lang/cl_ru.lua
@@ -1,14 +1,15 @@
-slib.usingDirectory('background_npcs_core/tool_options/ru')
+local target_directory = 'background_npcs_core/tool_options/lang/'
+slib.usingDirectory(target_directory .. 'ru')
 
 local lang = {}
 
-table.Merge(lang, include('ru/cl_options.lua'))
-table.Merge(lang, include('ru/cl_general.lua'))
-table.Merge(lang, include('ru/cl_client.lua'))
-table.Merge(lang, include('ru/cl_optimization.lua'))
-table.Merge(lang, include('ru/cl_spawn.lua'))
-table.Merge(lang, include('ru/cl_workshop.lua'))
-table.Merge(lang, include('ru/cl_actors.lua'))
-table.Merge(lang, include('ru/cl_modules.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_options.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_general.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_client.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_optimization.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_spawn.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_workshop.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_actors.lua'))
+table.Merge(lang, include(target_directory .. 'ru/cl_modules.lua'))
 
 return lang
