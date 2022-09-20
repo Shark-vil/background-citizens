35eb157e1757283fa00b9b8167bd375ba9bb823a
Shark-vil <shark_vil@mail.ru>
2022-09-20 21:09:52 +0300

    Added russian language for darkrp wanted notification

--- lua/background_npcs_core/config/gamemodes/sh_darkrp.lua
+++ lua/background_npcs_core/config/gamemodes/sh_darkrp.lua
@@ -1,6 +1,16 @@
 bgNPC.cfg.darkrp = {}
 
+-- TEXT --
+local text = slib.language({
+	['default'] = {
+		['notify'] = 'You cannot change your job while you are wanted!',
+	},
+	['russian'] = {
+		['notify'] = 'Вы не можете сменить работу, пока вас разыскивают!'
+	}
+})
+
 -- Prevents the player from changing job while he is wanted
 bgNPC.cfg.darkrp.disableChangeTeamByWanted = true
 -- Error text for limitation above
-bgNPC.cfg.darkrp.disableChangeTeamByWantedText = 'You cannot change your job while you are wanted!'
+bgNPC.cfg.darkrp.disableChangeTeamByWantedText = text['notify']
