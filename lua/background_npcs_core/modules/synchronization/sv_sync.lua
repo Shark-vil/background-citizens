b3e941cc5916e17a86ae4908770d925bcb70903d
Shark-vil <shark_vil@mail.ru>
2023-06-04 20:57:01 +0300

    Improved data synchronization between NPCs and players

--- lua/background_npcs_core/modules/synchronization/sv_sync.lua
+++ lua/background_npcs_core/modules/synchronization/sv_sync.lua
@@ -1,17 +1,26 @@
 local bgNPC = bgNPC
-local player_GetAll = player.GetAll
+local player_GetHumans = player.GetHumans
 local slib_GetHashSumm = slib.GetHashSumm
+local FrameTime = FrameTime
+local IsValid = IsValid
 --
-local sync_radius = 700
-local sync_distance = sync_radius ^ 2
+local current_pass = 0
+local function has_yield()
+	current_pass = current_pass + 1
+	if current_pass >= FrameTime() then
+		current_pass = 0
+		return true
+	end
+	return false
+end
 
-timer.Create('BGN_SynchronizationService', 1.5, 0, function()
+async.AddDedic('BGN_SynchronizationService', function(yield)
 	local sync_list = {}
 	local sync_list_count = 0
 
 	do
 		local actors = bgNPC:GetAll()
-		local players = player_GetAll()
+		local players = player_GetHumans()
 
 		for i = 1, #actors do
 			local actor = actors[i]
@@ -20,13 +29,13 @@ timer.Create('BGN_SynchronizationService', 1.5, 0, function()
 
 			if actor and actor:IsAlive() then
 				local npc = actor:GetNPC()
-				local pos = npc:GetPos()
 				for k = 1, #players do
 					local ply = players[k]
-					if ply:GetPos():DistToSqr(pos) <= sync_distance and ply:slibIsViewVector(pos) then
+					if IsValid(ply) and IsValid(npc) and ply:TestPVS(npc) then
 						sync_players_count = sync_players_count + 1
 						sync_players[sync_players_count] = ply
 					end
+					if has_yield() then yield() end
 				end
 			end
 
@@ -64,14 +73,17 @@ timer.Create('BGN_SynchronizationService', 1.5, 0, function()
 
 			for k = 1, #players do
 				local ply = players[k]
-				if ply and ply.snet_ready then
+				if ply and IsValid(ply) and ply.snet_ready then
 					local old_sync_hash = actor.sync_players_hash[ply] or -1
 					if sync_hash ~= old_sync_hash then
 						snet.Invoke('bgn_actor_sync_data', ply, actor.uid, sync_data)
 						actor.sync_players_hash[ply] = sync_hash
+						if has_yield() then yield() end
 					end
 				end
 			end
+
+			if has_yield() then yield() end
 		end
 	end
 end)
