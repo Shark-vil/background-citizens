355e6eae510522e1bb04b8af9eace5bddaf9b295
Shark-vil <shark_vil@mail.ru>
2023-07-24 00:02:50 +0300

    Change header options title

--- lua/background_npcs_core/tool_options/lang/en/cl_options.lua
+++ lua/background_npcs_core/tool_options/lang/en/cl_options.lua
@@ -3,13 +3,13 @@
 --]]
 return {
 	['bgn.settings.general_title'] = 'General',
-	['bgn.settings.client_title'] = 'Client',
-	['bgn.settings.spawn_title'] = 'Spawner',
-	['bgn.settings.states_title'] = 'Modules',
+	['bgn.settings.client_title'] = 'Local settings (Client)',
+	['bgn.settings.spawn_title'] = 'Spawn settings',
+	['bgn.settings.states_title'] = 'Extensions',
 	['bgn.settings.wanted_title'] = 'Wanted mode',
-	['bgn.settings.active_title'] = 'Actors',
-	['bgn.settings.workshop_title'] = 'Workshop',
+	['bgn.settings.active_title'] = 'Actors settings (NPC)',
+	['bgn.settings.workshop_title'] = 'Workshop (Publishing)',
 	['bgn.settings.optimization_title'] = 'Optimization',
-	['bgn.settings.generation_title'] = 'Generations',
-	['bgn.settings.addons_title'] = 'Addons',
+	['bgn.settings.generation_title'] = 'Real-time path generation',
+	['bgn.settings.addons_title'] = 'Supported addons',
 }
