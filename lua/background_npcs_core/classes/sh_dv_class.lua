9353df234e8b5018648f2d44661db80a2e2f012c
Shark-vil <sharkvil228@gmail.com>
2025-02-08 04:34:22 +0300

    Optimize class BGN_VEHICLE

--- lua/background_npcs_core/classes/sh_dv_class.lua
+++ lua/background_npcs_core/classes/sh_dv_class.lua
@@ -1,4 +1,16 @@
+local bgNPC = bgNPC
+local math_random = math.random
 local slib_GetUID = slib.GetUID
+local string_Explode = string.Explode
+local tonumber = tonumber
+local IsValid = IsValid
+local ipairs = ipairs
+local ents_Create = ents.Create
+local table_HasValueBySeq = table.HasValueBySeq
+local table_RemoveByValue = table.RemoveByValue
+local table_insert = table.insert
+local table_remove = table.remove
+--
 BGN_VEHICLE = {}
 
 function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
@@ -18,13 +30,13 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 	if vehicle_type == 'police' then
 		obj.ai_class = 'npc_dvbgn_police'
 		obj.ai_type = 'police'
-		obj.passengers_limit = math.random(1, 4)
+		obj.passengers_limit = math_random(1, 4)
 	elseif vehicle_type == 'taxi' then
 		obj.ai_class = 'npc_dvtaxi'
 		obj.ai_type = 'taxi'
 		obj.passengers_limit = 1
 	else
-		local vehicle_type_args = string.Explode('|', vehicle_type)
+		local vehicle_type_args = string_Explode('|', vehicle_type)
 
 		if (#vehicle_type_args > 1) then
 			obj.type = vehicle_type_args[1] ~= 'nil' and vehicle_type_args[1] or 'residents'
@@ -32,26 +44,26 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 			obj.ai_type = vehicle_type_args[3] ~= 'nil' and vehicle_type_args[3] or 'default'
 
 			if vehicle_type_args[4] and vehicle_type_args[4] ~= 'nil' then
-				local passengers_limit = string.Explode('-', vehicle_type_args[4])
+				local passengers_limit = string_Explode('-', vehicle_type_args[4])
 				if passengers_limit[1] and passengers_limit[2] then
-					obj.passengers_limit = math.random(tonumber(passengers_limit[1]), tonumber(passengers_limit[2]))
+					obj.passengers_limit = math_random(tonumber(passengers_limit[1]), tonumber(passengers_limit[2]))
 				else
 					obj.passengers_limit = tonumber(vehicle_type_args[4])
 				end
 			else
-				obj.passengers_limit = math.random(1, 4)
+				obj.passengers_limit = math_random(1, 4)
 			end
 		else
 			obj.ai_class = 'npc_decentvehicle'
 			obj.ai_type = 'default'
-			obj.passengers_limit = math.random(1, 4)
+			obj.passengers_limit = math_random(1, 4)
 		end
 	end
 
 	function obj:Initialize()
 		if self.ai or IsValid(self.ai) then return end
 		local vehicle = self.vehicle
-		local decentvehicle = ents.Create(self.ai_class)
+		local decentvehicle = ents_Create(self.ai_class)
 		decentvehicle:SetPos(vehicle:GetPos())
 		decentvehicle.DontUseSpawnEffect = true
 		decentvehicle.bgn_type = self.ai_type
@@ -87,7 +99,7 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 	end
 
 	function obj:AddPassenger(actor)
-		if table.HasValueBySeq(self.passengers, actor) then return false end
+		if table_HasValueBySeq(self.passengers, actor) then return false end
 		if self.passengers_limit ~= -1 and #self.passengers >= self.passengers_limit then return false end
 
 		if self:GetDriver() ~= actor then
@@ -96,7 +108,7 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 
 			for index, ent in ipairs(vehicle:GetChildren()) do
 				if ent:GetClass() == 'prop_vehicle_prisoner_pod' and not IsValid(ent:GetDriver()) then
-					local passenger = ents.Create('npc_decentvehicle_passenger')
+					local passenger = ents_Create('npc_decentvehicle_passenger')
 					passenger.Seat = ent
 					passenger.SeatIndex = index
 					passenger.actor = actor
@@ -106,7 +118,7 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 					passenger:SetPos(vehicle:GetPos())
 					passenger:Spawn()
 					passenger:Activate()
-					table.insert(self.passengers_models, passenger)
+					table_insert(self.passengers_models, passenger)
 					seats_are_taken = false
 					break
 				end
@@ -115,7 +127,7 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 			if seats_are_taken then return false end
 		end
 
-		table.insert(self.passengers, actor)
+		table_insert(self.passengers, actor)
 		return true
 	end
 
@@ -124,14 +136,14 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 			local model = self.passengers_models[k]
 			if IsValid(model) and model.actor == actor then
 				model:Remove()
-				table.remove(self.passengers_models, k)
+				table_remove(self.passengers_models, k)
 			end
 		end
 	end
 
 	function obj:RemovePassenger(actor)
 		self:RemovePassengerModel(actor)
-		table.RemoveByValue(self.passengers, actor)
+		table_RemoveByValue(self.passengers, actor)
 	end
 
 	function obj:GetPassengers()
@@ -143,7 +155,7 @@ function BGN_VEHICLE:Instance(vehicle_entity, vehicle_type, actor_type)
 		for i = #self.passengers, 1, -1 do
 			local actor = self.passengers[i]
 			if not actor or not actor:IsAlive() then
-				table.remove(self.passengers, i)
+				table_remove(self.passengers, i)
 			end
 		end
 	end
@@ -234,11 +246,11 @@ function BGN_VEHICLE:AddToList(vehicle_provider)
 		if other_vehicle_provider:GetVehicle() == vehicle_provider:GetVehicle() then return -1 end
 	end
 
-	return table.insert(bgNPC.DVCars, vehicle_provider)
+	return table_insert(bgNPC.DVCars, vehicle_provider)
 end
 
 function BGN_VEHICLE:RemoveFromList(vehicle_provider)
-	table.RemoveByValue(bgNPC.DVCars, vehicle_provider)
+	table_RemoveByValue(bgNPC.DVCars, vehicle_provider)
 end
 
 function BGN_VEHICLE:GetVehicleProvider(vehicle)
